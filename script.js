5cfd26cbebe8d5fb26107cf317c0fd0df618e803
adelclra <adelclangitan@gmail.com>
2024-04-15 16:05:53 +0800

    update portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleContent(titleElement) {
+    var content = titleElement.nextElementSibling;
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
+
