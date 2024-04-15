b1743cb8ec8de87514715cffde246e1556c86856
adelclra <adelclangitan@gmail.com>
2024-04-15 16:59:20 +0800

    update 2

--- script.js
+++ script.js
@@ -1,9 +1,43 @@
-function toggleContent(titleElement) {
-    var content = titleElement.nextElementSibling;
-    if (content.style.display === "none" || content.style.display === "") {
-        content.style.display = "block";
-    } else {
-        content.style.display = "none";
-    }
+
+function toggleContent(element) {
+    const content = element.nextElementSibling;
+    content.style.display === 'none' ? content.style.display = 'block' : content.style.display = 'none';
 }
 
+function toggleDescription(image) {
+    const description = image.nextElementSibling;
+    description.style.display === 'none' ? description.style.display = 'block' : description.style.display = 'none';
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle input');
+    const navContent = document.querySelector('.nav-content');
+
+    menuToggle.addEventListener('click', function() {
+        navContent.classList.toggle('active');
+    });
+
+
+    const navLinks = document.querySelectorAll('.nav-content a');
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            navContent.classList.remove('active');
+        });
+    });
+
+    const scrollLinks = document.querySelectorAll('.nav-content a[href^="#"]');
+    scrollLinks.forEach(function(link) {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const target = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: target.offsetTop,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
